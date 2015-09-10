093169e99764b65a76c4d6076dccad42196e3fde
Hanuma <suryahanuma@gmail.com>
2015-09-10 16:07:45 +0530

    Initial Commit
    First Commit - An example on how to use entity framework database first
    approach/edmx creation

---
+++ DBFirstSample/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DBFirstSample.Startup))]
+namespace DBFirstSample
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
